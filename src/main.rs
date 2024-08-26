c648e70cf87a0d50a6294f8916cf7fce4f847f64
sea <api@sea.com>
2024-08-26 08:21:59 +0800

    添加config

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-pub mod webrtc;
 pub mod control;
+pub mod webrtc;
 
 use anyhow::Result;
 // use serde::Deserialize;
 
 use axum::{
-    response::{Html}, 
-    routing::{get, post, get_service}, 
-    Json, Router
+    response::Html,
+    routing::{get, get_service, post},
+    Json, Router,
 };
 use tokio::sync::oneshot;
 use tower_http::services::ServeDir;
 
 async fn serve_html() -> impl axum::response::IntoResponse {
     // Html(include_str!("C:/Users/Administrator/Desktop/webrtc/rtp_to_webrtc.html"))
-    let html_content = std::fs::read_to_string("web/index.html")
-    .expect("Failed to read HTML file");
+    let html_content = std::fs::read_to_string("web/index.html").expect("Failed to read HTML file");
 
     // 返回 HTML 响应
     Html(html_content)
@@ -26,40 +25,46 @@ struct ResponseData {
 }
 
 async fn recv_and_send_possession(Json(payload): Json<serde_json::Value>) -> Json<ResponseData> {
-
-    let (tx,rx) = oneshot::channel();
-    let (tx2,rx2) = oneshot::channel();
+    let (tx, rx) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
 
     let user_session = payload.get("data").unwrap();
     if let Some(s) = user_session.as_str() {
         tx.send(s.to_string());
     } else {
-        return Json(ResponseData {data: "Invaild SDP".to_string(),})
+        return Json(ResponseData {
+            data: "Invaild SDP".to_string(),
+        });
     }
     tokio::spawn(crate::webrtc::connect::connect_and_rtp((tx2, rx)));
     match rx2.await {
         Ok(data) => {
             // println!("{}", data);
-            return Json(ResponseData {r#data: data,})
-        },
+            return Json(ResponseData { r#data: data });
+        }
         Err(_) => {
             eprintln!("Invaild receive SDP");
-            return Json(ResponseData {r#data: "can not get SDP".to_string(),});
+            return Json(ResponseData {
+                r#data: "can not get SDP".to_string(),
+            });
         }
     }
-
 }
 #[tokio::main]
 async fn main() -> Result<()> {
+    let settings = config::Config::builder()
+        .add_source(config::File::with_name("config.toml"))
+        .build()
+        .unwrap();
+
     let app = Router::new()
         // .route("/", get(serve_html))
         .route("/post", post(recv_and_send_possession))
         .nest_service("/", get_service(ServeDir::new("./web")));
 
-    // let listener = tokio::net::TcpListener::bind("192.168.50.34:3000")
-    let listener = tokio::net::TcpListener::bind("[240e:3b6:30f1:16b0:5871:4e46:37b6:b699]:3000")
-    .await
-    .unwrap();
+    let listener = tokio::net::TcpListener::bind(settings.get_string("addr").unwrap())
+        .await
+        .unwrap();
 
     axum::serve(listener, app).await.unwrap();
 
